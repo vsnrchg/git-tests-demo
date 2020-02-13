bccf62e63714cbfb906605c776f750823ebae92b
vsnrchg <vsnrchg@gmail.com>
2020-02-13 10:35:40 +0530

    first commit

---
+++ src/main/java/com/twlabs/gittests/demo/HelloController.java
@@ -0,0 +1,15 @@
+package com.twlabs.gittests.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloController  {
+
+    @GetMapping
+    public String  sayHello(String helloMessage) {
+        return "Hello " + helloMessage;
+    }
+}
---
+++ src/test/java/com/twlabs/gittests/demo/HelloControllerTest.java
@@ -0,0 +1,18 @@
+package com.twlabs.gittests.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class HelloControllerTest {
+    @Autowired
+    HelloController helloController;
+    @Test
+    public void testSayHello(){
+        String result = helloController.sayHello("World");
+        assertEquals("Hello World", result);
+    }
+}
